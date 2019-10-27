07a6066ead1b6ae5f569ebaddb65d9d48132025a
Sanyam Dogra <sdlord07@gmail.com>
2019-10-27 12:58:52 +0530

    Hide graph for smaller screen sizes

--- src/components/OdiStats/TimelineODI.js
+++ src/components/OdiStats/TimelineODI.js
@@ -10,9 +10,10 @@ import {
 import { Container } from "react-bootstrap";
 
 const styles = {
-  containerClass: {"padding-top":"3%","padding-bottom":"3%"},
+  containerClass: { "padding-top": "3%", "padding-bottom": "3%" },
   colorClass: { color: "#2026D2" },
   boldClass: { "font-weight": "900" },
+  warningClass:{ color: "red", "font-weight": "900"},
   imageContainerClass: { "padding-top": "20px" },
   labelClass: {
     position: "absolute",
@@ -25,23 +26,28 @@ export class TimelineODI extends Component {
   render() {
     return (
       <div>
-        <Container style={styles.containerClass}>
-          <LineChart width={1000} height={250} data={this.props.data}>
-            <XAxis dataKey="name" />
-            <YAxis domain={[0, 200]} />
-            <Tooltip />
-            <CartesianGrid stroke="#eee" strokeDasharray="12 12" />
-            <Line
-              type="monotone"
-              dataKey="runs"
-              stroke="#2026D2"
-              strokeWidth="3"
-              strokeOpacity="0.85"
-              activeDot={{ r: 8 }}
-            />
-          </LineChart>
-          <div style={styles.labelClass}>Total Matches</div>
-        </Container>
+        <div className="d-xl-none pt-3 red" style={styles.warningClass}>
+          Please switch to a larger screen to view the Timeline!
+        </div>
+        <div className="d-none d-md-block ">
+          <Container style={styles.containerClass}>
+            <LineChart width={1000} height={250} data={this.props.data}>
+              <XAxis dataKey="name" />
+              <YAxis domain={[0, 200]} />
+              <Tooltip />
+              <CartesianGrid stroke="#eee" strokeDasharray="12 12" />
+              <Line
+                type="monotone"
+                dataKey="runs"
+                stroke="#2026D2"
+                strokeWidth="3"
+                strokeOpacity="0.85"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+            <div style={styles.labelClass}>Total Matches</div>
+          </Container>
+        </div>
       </div>
     );
   }
