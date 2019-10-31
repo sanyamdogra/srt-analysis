534c443e272c4e3bd297fe179f93042643a17102
Sanyam Dogra <sdlord07@gmail.com>
2019-10-31 13:05:41 +0530

    Fixed footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HeaderNav from "./components/Header/HeaderNav";
 import QuoteBoard from "./components/Quote/QuoteBoard";
 import TotalStats from "./components/OdiStats/TotalStats";
 import TeamAnalysis from "./components/TeamAnalysis/TeamAnalysis";
-import Footer from "./components/Footer/Footer"
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -15,9 +15,9 @@ function App() {
       <div className="App">
         <QuoteBoard />
         <TotalStats />
-        <TeamAnalysis/>
+        <TeamAnalysis />
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
